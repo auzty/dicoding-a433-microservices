6fb19620a6de915517a03b05ee25b15d8db07560
yusuf <thea.uzty@gmail.com>
2022-11-29 16:31:33 +0700

    Adding script to build and push

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# membuat docker image
+docker build -t ghcr.io/auzty/karsajobs-ui:latest .
+
+# login ke github packages
+echo $PASSWORD_DOCKER_HUB | docker login ghcr.io -u auzty --password-stdin
+
+# mengunggah image ke github packages
+docker push ghcr.io/auzty/karsajobs-ui:latest
